8a7e7803afe753f019b5030d1db287bd0882ca22
ycalk <alosev752@gmail.com>
2025-07-31 21:53:49 +0500

    Update folders schema: remove 'schema' folder

--- additional/shared_models/src/shared_models/messaging/models/__init__.py
+++ additional/shared_models/src/shared_models/messaging/models/__init__.py
@@ -1,3 +1,4 @@
+from .message import Message
 from .message_notification import MessageNotification
 from .message_shown import MessageShown
 from .moderation_result import ModerationResult
@@ -5,6 +6,7 @@ from .no_available_time import NoAvailableTime
 
 
 __all__ = [
+    "Message",
     "MessageNotification",
     "MessageShown",
     "ModerationResult",
--- additional/shared_models/src/shared_models/messaging/models/message_notification.py
+++ additional/shared_models/src/shared_models/messaging/models/message_notification.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, Field
 from typing import Annotated
-from ...message import Message
+from .message import Message
 
 
 class MessageNotification(BaseModel):
