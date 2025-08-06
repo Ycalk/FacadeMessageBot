1fcf712598defcdca2e0be2e80ed32b65c0109e3
ycalk <alosev752@gmail.com>
2025-08-07 01:58:58 +0500

    Add admin commands for manage moderators

--- services/manual_moderator/src/manual_moderator/filters/admin_state.py
+++ services/manual_moderator/src/manual_moderator/filters/admin_state.py
@@ -5,3 +5,7 @@ class AdminState(StatesGroup):
     add_admin_set_telegram_id = State()
     remove_admin_set_telegram_id = State()
     remove_admin_confirm = State()
+
+    add_moderator_set_telegram_id = State()
+    remove_moderator_set_telegram_id = State()
+    remove_moderator_confirm = State()
--- services/manual_moderator/src/manual_moderator/handlers/start.py
+++ services/manual_moderator/src/manual_moderator/handlers/start.py
@@ -2,7 +2,7 @@ from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
-from manual_moderator.utils import Admin, Texts
+from manual_moderator.utils import Admin, Texts, Moderator
 
 start_router = Router()
 
@@ -20,3 +20,12 @@ async def cmd_start(message: Message, state: FSMContext):
             last_name=message.from_user.last_name,
         ).save()
         await message.answer(Texts.Messages.admin_start)
+
+    if await Moderator.get_or_none(message.from_user.id):
+        await Moderator(
+            telegram_id=message.from_user.id,
+            first_name=message.from_user.first_name,
+            username=message.from_user.username,
+            last_name=message.from_user.last_name,
+        ).save()
+        await message.answer(Texts.Messages.moderator_start)
