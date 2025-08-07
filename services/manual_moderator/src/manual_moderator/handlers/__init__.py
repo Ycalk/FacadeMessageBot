534dc8b170586dd566be06d2ee8971e944038172
ycalk <alosev752@gmail.com>
2025-08-07 22:15:36 +0500

    Update admin commands: add auto approve and bot status

--- services/manual_moderator/src/manual_moderator/handlers/__init__.py
+++ services/manual_moderator/src/manual_moderator/handlers/__init__.py
@@ -1,5 +1,6 @@
 from .start import start_router
 from .admin import admin_router
+from .moderator import moderator_commands_router
 
 
-__all__ = ["start_router", "admin_router"]
+__all__ = ["start_router", "admin_router", "moderator_commands_router"]
