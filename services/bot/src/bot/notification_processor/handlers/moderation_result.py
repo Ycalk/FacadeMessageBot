4d206b66f39e8472f55de175246f8cf482532957
ycalk <alosev752@gmail.com>
2025-08-10 18:04:46 +0500

    Integrate manual moderator into bot

--- services/bot/src/bot/notification_processor/handlers/moderation_result.py
+++ services/bot/src/bot/notification_processor/handlers/moderation_result.py
@@ -6,13 +6,13 @@ from shared_models.messaging import (
     bot_moderate_response_queue,
     ModerationResult,
 )
+from aiomax import Bot
 from faststream import Context
 from shared_models.enums import ModeratorType, MessageState
 from shared_models.enums import ModerationResult as ModerationResultEnum
 from aiomax.methods import SendMessage
 from aiomax.types import TextFormat
-from shared_models.database import Message
-from bot.bot import bot
+from shared_models.database import Message, ModerationLog
 from bot.utils import Texts
 
 
@@ -21,7 +21,9 @@ moderation_result_router = RabbitRouter()
 
 @moderation_result_router.subscriber(bot_moderate_response_queue, bot_exchange)
 async def moderation_result_handler(
-    moderation_result: ModerationResult, logger: Logger = Context()
+    moderation_result: ModerationResult,
+    logger: Logger = Context(),
+    bot: Bot = Context(),
 ) -> None:
     async with in_transaction():
         message = await Message.get_or_none(
@@ -34,15 +36,52 @@ async def moderation_result_handler(
             return
         if moderation_result.result == ModerationResultEnum.APPROVED:
             if moderation_result.source == ModeratorType.AUTO:
-                message.state = MessageState.PENDING_MANUAL_MODERATION
-                await message.save()
-                await bot(
-                    SendMessage(
-                        user_id=message.user.max_id,
-                        text=Texts.Messages.auto_moderation_completed,
-                        text_format=TextFormat.MARKDOWN,
+                if message.state == MessageState.PENDING_AUTO_MODERATION:
+                    message.state = MessageState.PENDING_MANUAL_MODERATION
+                    await message.save()
+
+                    await ModerationLog.create(
+                        message=message,
+                        source=moderation_result.source,
+                        result=moderation_result.result,
+                        reason=moderation_result.reason,
                     )
-                )
+
+                    await bot(
+                        SendMessage(
+                            user_id=message.user.max_id,
+                            text=Texts.Messages.auto_moderation_completed,
+                            text_format=TextFormat.MARKDOWN,
+                        )
+                    )
+                else:
+                    logger.warning(
+                        f"Message {message.id} is not in pending auto-moderation state. Current state: {message.state}"
+                    )
+
+            elif moderation_result.source == ModeratorType.MANUAL:
+                if message.state == MessageState.PENDING_MANUAL_MODERATION:
+                    message.state = MessageState.PENDING_MEDIA_FACADE_MODERATION
+                    await message.save()
+
+                    await ModerationLog.create(
+                        message=message,
+                        source=moderation_result.source,
+                        result=moderation_result.result,
+                        reason=moderation_result.reason,
+                    )
+
+                    await bot(
+                        SendMessage(
+                            user_id=message.user.max_id,
+                            text=Texts.Messages.manual_moderation_completed,
+                            text_format=TextFormat.MARKDOWN,
+                        )
+                    )
+                else:
+                    logger.warning(
+                        f"Message {message.id} is not in pending manual-moderation state. Current state: {message.state}"
+                    )
         else:
             message.state = MessageState.REJECTED
             await message.save()
