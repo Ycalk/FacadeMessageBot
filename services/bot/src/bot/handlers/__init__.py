5f215f42d640c7360e967c5ceba0088d92366d95
ycalk <alosev752@gmail.com>
2025-08-12 23:50:44 +0500

    Make field name required

--- services/bot/src/bot/handlers/__init__.py
+++ services/bot/src/bot/handlers/__init__.py
@@ -10,8 +10,6 @@ from .start import (
 from .get_message import get_message, get_message_filter
 from .get_name import get_name, get_name_filter
 from .city import (
-    add_city_solution,
-    add_city_solution_filter,
     get_city,
     get_city_filter,
     confirm_city,
@@ -36,8 +34,6 @@ __all__ = [
     "get_message_filter",
     "get_name",
     "get_name_filter",
-    "add_city_solution",
-    "add_city_solution_filter",
     "get_city",
     "get_city_filter",
     "confirm_city",
