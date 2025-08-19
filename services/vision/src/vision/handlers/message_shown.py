3de580f03767f21f23626d365f9b87e303acff65
ycalk <alosev752@gmail.com>
2025-08-20 00:38:25 +0500

    Add message shown handler

---
+++ services/vision/src/vision/handlers/message_shown.py
@@ -0,0 +1,33 @@
+from faststream.rabbit import RabbitRouter
+from logging import Logger
+from shared_models.messaging.queues.vision import (
+    vision_notification_queue,
+    vision_notification_dlx_queue,
+)
+from shared_models.messaging.exchanges import (
+    vision_exchange,
+    dlx_exchange,
+)
+from vision.utils.storage.base import BaseStorage
+from vision.utils.storage.models import ShownMessage
+from shared_models.messaging import MessageInput
+from faststream import Context
+
+
+message_shown_router = RabbitRouter()
+
+dlx = message_shown_router.publisher(vision_notification_dlx_queue, dlx_exchange)
+
+
+@message_shown_router.subscriber(vision_notification_queue, vision_exchange)
+async def moderate(
+    message_input: MessageInput,
+    logger: Logger = Context(),
+    storage: BaseStorage = Context(),
+):
+    logger.info(f"Received shown message {message_input.message.message_id}.")
+    await storage.save_shown_message(
+        ShownMessage(
+            message=message_input.message,
+        )
+    )
